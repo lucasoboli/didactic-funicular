0d9ae7d039987923ce66753d22f97243ecdffed0
lucasoboli <lucasoboli@gmail.com>
2018-10-25 10:14:34 -0300

    Updated client and server

--- client.cpp
+++ client.cpp
@@ -1,62 +1,70 @@
 /*
     C ECHO client example using sockets
 */
-#include <arpa/inet.h>  //inet_addr
-#include <stdio.h>      //printf
-#include <string.h>     //strlen
-#include <sys/socket.h> //socket
-#include <unistd.h>
 
-#include <fstream>
 #include <iostream>
 #include <string>
+#include <fstream>
+#include <stdio.h>      //printf
+#include <string.h>     //strlen
+#include <unistd.h>
+#include <arpa/inet.h>  //inet_addr
+#include <sys/socket.h> //socket
 
 using namespace std;
 
-int main(int argc, char *argv[]) {
+
+int main (int argc, char *argv[])
+{
   int sock;
   struct sockaddr_in server;
   char buffer[1024];
+
   string command, arg;
 
   string serverLocation = argv[1];
   string serverPort = argv[2];
   string userName = argv[3];
 
-  cout << "Server Location: " << serverLocation << endl;
-  cout << "Server Port: " << serverPort << endl;
-  cout << "Username: " << userName << endl;
+  cout << "> Server Location: " << serverLocation << endl;
+  cout << "> Server Port: " << serverPort << endl;
+  cout << "> Username: " << userName << endl;
 
-  // Create socket
+  // Creates socket
   sock = socket(AF_INET, SOCK_STREAM, 0);
-  if (sock == -1) {
-    printf("Could not create socket");
-  }
-  puts("Socket created");
+
+  if (sock == -1)
+    printf(">> Could not create socket\n\n");
+
+  puts(">> Socket created\n");
 
   server.sin_addr.s_addr = inet_addr(serverLocation.c_str());
   server.sin_family = AF_INET;
-  server.sin_port = htons(stoi(serverPort.c_str()));
+  server.sin_port = htons(stoi(serverPort.c_str() ));
 
   // Connect to remote server
-  if (connect(sock, (struct sockaddr *)&server, sizeof(server)) < 0) {
-    perror("connect failed. Error");
+  if (connect(sock, (struct sockaddr *)&server, sizeof(server)) < 0)
+  {
+    perror(">> Connection failed. Error\n\n");
     return 1;
   }
 
-  cout << "Conectado ao servidor!" << endl << endl;
+  cout << ">> Connected to the server!\n" << endl << endl;
+
   send(sock, userName.c_str(), userName.size(), 0);
 
-  // keep communicating with server
-  while (1) {
-    cout << ">";
+  // Keep communicating with the server
+  while (1)
+  {
+    cout << "> ";
     cin.clear();
 
-    // Lê o comando
+    // Read the command
     cin >> command;
 
-    if (command.compare("exit") == 0) {
-      cout << "Bye!" << endl;
+    if (command.compare("exit") == 0)
+    {
+      cout << ">>> Bye!" << endl << endl;
       close(sock);
       return 0;
     } else if (command.compare("echo") == 0) {
@@ -64,10 +72,11 @@ int main(int argc, char *argv[]) {
 
       send(sock, command.c_str(), command.size() + 1, 0);
 
-      for (float i = 0; i < 100000; i++)
-        ; // delay para mandar a segunda parte da mensagem
+      for (float i=0; i<100000; i++)
+        ; // Delay to send second part of message
 
-      send(sock, arg.c_str(), arg.size() + 1, 0);
+      send(sock, arg.c_str(), arg.size()+1, 0);
+
     } else if (command.compare("send") == 0) {
       string endOF = "!EOF!";
       string line;
@@ -76,51 +85,55 @@ int main(int argc, char *argv[]) {
 
       ifstream myfile(arg);
 
-      send(sock, command.c_str(), command.size() + 1, 0);
+      send(sock, command.c_str(), command.size()+1, 0);
 
-      for (float i = 0; i < 100000; i++)
-        ; // delay
+      for (float i=0; i<100000; i++)
+        ; // Delay
 
-      send(sock, arg.c_str(), arg.size() + 1, 0); // Manda o nome do arquivo
+      send(sock, arg.c_str(), arg.size()+1, 0); // Send the file's name
 
-      sleep(1); // Delay pro server ficar pronto pra esperar as mensagens
+      sleep(1); // Delay so that the server is ready to receive messages
 
-      if (myfile.is_open()) {
-        while (getline(myfile, line)) {
-
+      if (myfile.is_open())
+      {
+        while (getline(myfile, line))
+        {
           line += '\0';
-          send(sock, line.c_str(), line.size() + 1, 0); // Envia uma linha
-          recv(sock, buffer, 1024,
-               0); // Espera o ACK do server que recebeu a linha
+          send(sock, line.c_str(), line.size()+1, 0); // Send one line
+          recv(sock, buffer, 1024, 0); // Wait server's ACK that received the line
           memset(buffer, 0, 1024);
         }
+
         myfile.close();
 
-        for (float i = 0; i < 500000; i++)
-          ; // delay para mandar o fim do arquivo
+        for (float i=0; i<500000; i++)
+          ; // Delay to send the EOF
 
-        send(sock, endOF.c_str(), endOF.size() + 1, 0);
+        send(sock, endOF.c_str(), endOF.size()+1, 0);
       }
-    } else if (command.compare("list") == 0) {
-      send(sock, command.c_str(), command.size() + 1, 0);
 
-      while (1) {
+    } else if (command.compare("list") == 0) {
+      send(sock, command.c_str(), command.size()+1, 0);
+
+      while (1)
+      {
         recv(sock, buffer, 1024, 0);
 
         arg = buffer;
 
-        if (arg.compare("!EOF!") == 0) { // Se for o final da lista, sai
+        if (arg.compare("!EOF!") == 0) // Break if it's the end of the list
+        {
           memset(buffer, 0, 1024);
           break;
         }
 
         cout << arg << endl;
 
-        send(sock, "ACK\0", 4, 0); // Manda um ACK
+        send(sock, "ACK\0", 4, 0); // Send ACK
         memset(buffer, 0, 1024);
       }
     } else if (command.compare("get") == 0) {
-      send(sock, command.c_str(), command.size() + 1, 0);
+      send(sock, command.c_str(), command.size()+1, 0);
 
       cin >> arg;
 
@@ -129,31 +142,34 @@ int main(int argc, char *argv[]) {
 
       nomeDoArquivo = arg;
 
-      for (float i = 0; i < 100000; i++)
-        ; // delay
+      for (float i=0; i<100000; i++)
+        ; // Delay
 
-      send(sock, arg.c_str(), arg.size() + 1, 0); // Manda o nome do arquivo
+      send(sock, arg.c_str(), arg.size() + 1, 0); // Send the file's name
 
       myfile.open(nomeDoArquivo);
 
       memset(buffer, 0, 1024);
 
 			int lines = 0;
-      while (1) {
+
+      while (1)
+      {
 				lines++;
         recv(sock, buffer, 1024, 0);
 
         arg = buffer;
         //cout << arg << endl;
 
-        if (arg.compare("!EOF!") == 0) {
+        if (arg.compare("!EOF!") == 0)
+        {
           memset(buffer, 0, 1024);
           break;
         }
 
-        myfile << arg << endl; // Recebe a linha
+        myfile << arg << endl; // Receives the line
 
-        send(sock, "ACK\0", 4, 0); // Manda um ACK
+        send(sock, "ACK\0", 4, 0); // Send ACK
         memset(buffer, 0, 1024);
       }
 
@@ -162,8 +178,9 @@ int main(int argc, char *argv[]) {
 
       myfile.close();
       memset(buffer, 0, 1024);
+
     } else {
-      cout << "Comando desconhecido!" << endl;
+      cout << ">> Unknown command!\n" << endl;
       continue;
     }
 
@@ -173,7 +190,7 @@ int main(int argc, char *argv[]) {
       break;
     }
 
-    cout << "SERVER: " << buffer << endl;
+    cout << endl << ">> SERVER: " << buffer << endl;
 
     memset(buffer, 0, 1024);
   }
--- server.cpp
+++ server.cpp
@@ -1,121 +1,126 @@
 /*
-    C socket server example
+    C server socket example
 */
 
-#include <arpa/inet.h> //inet_addr
-#include <pthread.h>
-#include <pthread.h>
-#include <stdio.h>
+#include <iostream>
 #include <string.h> //strlen
-#include <sys/socket.h>
+#include <fstream>
+#include <stdio.h>
 #include <unistd.h> //write
+#include <arpa/inet.h> //inet_addr
+#include <sys/socket.h>
 
 #include <algorithm>
-#include <fstream>
-#include <iostream>
 #include <map>
 #include <thread>
 #include <vector>
 
-void *myThreadFun(void *vargp) {
-  sleep(1);
-  printf("Printing GeeksQuiz from Thread \n");
-  return NULL;
-}
 
 using namespace std;
 
-void worker() {
-  while (1) {
+void worker()
+{
+  while (1)
+  {
     cout << "te thread" << endl;
     sleep(1);
   }
 }
 
-int main(int argc, char *argv[]) {
+
+int main(int argc, char *argv[])
+{
   vector<thread> workers;
   map<string, vector<string>> userMap;
 
   int socket_desc, client_sock, c;
   struct sockaddr_in server;
-
   string serverPort = argv[1];
 
-  // Create socket
+  // Creates socket
   socket_desc = socket(AF_INET, SOCK_STREAM, 0);
-  if (socket_desc == -1) {
-    printf("Could not create socket");
-  }
-  puts("Socket created");
 
-  // Prepare the sockaddr_in structure
+  if (socket_desc == -1)
+    printf(">> Could not create socket\n\n");
+
+  puts(">> Socket created\n");
+
+  // Prepares the sockaddr_in structure
   server.sin_family = AF_INET;
   server.sin_addr.s_addr = INADDR_ANY;
   server.sin_port = htons(stoi(serverPort.c_str()));
 
   // Bind
-  if (bind(socket_desc, (struct sockaddr *)&server, sizeof(server)) < 0) {
-    // print the error message
-    perror("bind failed. Error");
+  if (bind(socket_desc, (struct sockaddr *)&server, sizeof(server)) < 0)
+  {
+    perror(">> Bind failed. Error");   // Print the error message
     return 1;
   }
-  puts("bind done");
 
-  // Listen
+  puts(">> Bind done\n");
+
+  // Listen for connections
   listen(socket_desc, 3);
 
-  // Accept and incoming connection
-  puts("Waiting for incoming connections...");
+  // Accept an incoming connection
+  puts(">> Waiting for incoming connections...\n");
   c = sizeof(struct sockaddr_in);
 
-  // accept connection from an incoming client
+  // Accept connection from an incoming client
   struct sockaddr_in client;
 
-  while (1) {
-    // Aceita uma conexão socket e cria uma thread com seu identificador
-    client_sock =
-        accept(socket_desc, (struct sockaddr *)&client, (socklen_t *)&c);
+  while (1)
+  {
+    // Accepts a socket connection and creates a thread with it's ID
+    client_sock = 
+    accept(socket_desc, (struct sockaddr *)&client, (socklen_t *)&c);
 
-    workers.push_back(thread([&userMap, client_sock, client, socket_desc]() {
+    workers.push_back(thread([&userMap, client_sock, client, socket_desc]()
+    {
       int readSize;
       char clientName[20];
       char bufferC[1024];
       char bufferA[1024];
       string clientCommand, clientArg;
 
-      if (client_sock < 0) {
-        perror("accept failed");
+      if (client_sock < 0)
+      {
+        perror(">>> Accept failed\n\n");
         return 1;
       }
 
-      puts("Connection accepted");
+      puts(">>> Connection accepted\n");
 
       // Lê o nome do cliente do socket
       recv(client_sock, clientName, 20, 0);
-      cout << "Conectado cliente: " << clientName << endl;
+      cout << ">>>> Connected to the client: " << clientName << endl;
 
-      while (1) {
+      while (1)
+      {
         readSize = recv(client_sock, bufferC, 1024, 0);
 
-        // Checa se for uma desconexão. Se for dá flush e break
-        if (readSize == 0) {
-          cout << "Cliente " << clientName << " desconectado!" << endl;
+        // Checks for disconnect. If true, flushe and break
+        if (readSize == 0)
+        {
+          cout << ">>>> Client " << clientName << " disconnected!" << endl << endl;
           fflush(stdout);
           break;
         } else if (readSize == -1) {
-          perror("recv failed");
+          perror(">>>> recv failed\n\n");
         }
 
-        // Se não pega e trata o comando recebido
+        // If not disconnected, treats the received command
         clientCommand = bufferC;
         cout << "(" << clientName << ") COMANDO RECEBIDO: " << clientCommand
              << endl;
 
-        if (clientCommand.compare("echo") == 0) {
-          // Lê o argumento do echo
+        if (clientCommand.compare("echo") == 0)
+        {
+          // Reads echo's argument
           recv(client_sock, bufferA, 1024, 0);
           clientArg = bufferA;
-          write(client_sock, clientArg.c_str(), clientArg.size() + 1);
+
+          write(client_sock, clientArg.c_str(), clientArg.size()+1);
 
           memset(bufferC, 0, 1024);
           memset(bufferA, 0, 1024);
@@ -131,67 +136,70 @@ int main(int argc, char *argv[]) {
 
           memset(bufferA, 0, 1024);
 
-          while (1) {
+          while (1)
+          {
             recv(client_sock, bufferA, 1024, 0);
 
             clientArg = bufferA;
             //cout << clientArg << endl;
 
-            if (clientArg.compare("!EOF!") == 0) {
+            if (clientArg.compare("!EOF!") == 0)
+            {
               memset(bufferA, 0, 1024);
               break;
             }
 
-            myfile << clientArg << endl; // Recebe a linha
+            myfile << clientArg << endl; // Receives the line
 
-            write(client_sock, "ACK\0", 4); // Manda um ACK
+            write(client_sock, "ACK\0", 4); // Send ACK
             memset(bufferA, 0, 1024);
           }
 
           myfile.close();
 
-          string done = "Arquivo inserido com sucesso";
+          string done = "file saved successfully!\n";
 
           userMap[clientName].push_back(nomeDoArquivo.substr(4));
 
-          for (float i = 0; i < 100000; i++)
+          for (float i=0; i<100000; i++)
             ; // delay para mandar o fim do arquivo
 
-          write(client_sock, done.c_str(), done.size() + 1);
+          write(client_sock, done.c_str(), done.size()+1);
 
           memset(bufferC, 0, 1024);
           memset(bufferA, 0, 1024);
-        } else if (clientCommand.compare("list") == 0) {
-          write(client_sock, "============", 15); // Header da lista
+        } else if (clientCommand.compare("list") == 0)
+        {
+          write(client_sock, "============", 15); // List's header
           recv(client_sock, bufferA, 1024,
-               0); // Espera o ACK do cliente que recebeu a linha
+               0); // Waits for the client's ACK which received the line
 
-          for (auto nomeDoArquivo : userMap[clientName]) {
+          for (auto nomeDoArquivo : userMap[clientName])
+          {
             for (float i = 0; i < 100000; i++)
-              ; // delay para mandar cada linha da lista
+              ; // Delay to send each line of the list
 
             nomeDoArquivo = "* " + nomeDoArquivo;
+
             write(client_sock, nomeDoArquivo.c_str(), nomeDoArquivo.size() + 1);
-            recv(client_sock, bufferA, 1024,
-                 0); // Espera o ACK do cliente que recebeu a linha
+            recv(client_sock, bufferA, 1024,0);  // Waits for the client's ACK which received the line
           }
-          write(client_sock, "============", 15); // Bunda da lista
-          recv(client_sock, bufferA, 1024,
-               0); // Espera o ACK do cliente que recebeu a linha
+
+          write(client_sock, "============", 15); // List's ass
+          recv(client_sock, bufferA, 1024, 0); // Waits for the client's ACK which received the line
           string endOF = "!EOF!";
 
-          for (float i = 0; i < 100000; i++)
-            ; // delay para mandar o fim do arquivo
+          for (float i=0; i<100000; i++)
+            ; // Delay to send the EOF
 
-          write(client_sock, endOF.c_str(), endOF.size() + 1);
+          write(client_sock, endOF.c_str(), endOF.size()+1);
 
-          string done = "Mostrar lista concluído!";
-          for (float i = 0; i < 100000; i++)
-            ; // delay para
-              // mandar o fim do
-              // arquivo
+          string done = "Completed list show!\n\n";
+          for (float i=0;  i<100000; i++)
+            ; // Delay to send the EOF
 
-          write(client_sock, done.c_str(), done.size() + 1);
+          write(client_sock, done.c_str(), done.size()+1);
+
         } else if (clientCommand.compare("get") == 0) {
           string endOF = "!EOF!";
           string nomeDoArquivo;
@@ -200,64 +208,65 @@ int main(int argc, char *argv[]) {
           nomeDoArquivo = bufferA;
           nomeDoArquivo = "SERV" + nomeDoArquivo;
 
-          cout << "NOME DO ARQUIVO: " << nomeDoArquivo << endl;
+          cout << ">>> NOME DO ARQUIVO: " << nomeDoArquivo << endl;
 
           // Checa se o usuário tem o arquivo na lista dele
           bool temArquivo = false;
-          for (auto arquivo : userMap[clientName]) {
+          for (auto arquivo : userMap[clientName])
+          {
             if (arquivo.compare(nomeDoArquivo.substr(4)) == 0)
               temArquivo = true;
           }
 
-          if (!temArquivo) {
-            for (float i = 0; i < 500000; i++)
-              ; // delay para mandar o fim do arquivo
+          if (!temArquivo)
+          {
+            for (float i=0; i<500000; i++)
+              ; // Delay to send the EOF
 
-            send(client_sock, endOF.c_str(), endOF.size() + 1, 0);
+            send(client_sock, endOF.c_str(), endOF.size()+1, 0);
 
-            string done = "Você não possui este arquivo!";
-            for (float i = 0; i < 100000; i++)
-              ; // delay para
-                // mandar o fim do
-                // arquivo
+            string done = "You do not own this file!\n\n";
 
-            write(client_sock, done.c_str(), done.size() + 1);
+            for (float i=0; i<100000; i++)
+              ; // Delay to send the EOF
+
+            write(client_sock, done.c_str(), done.size()+1);
+            continue;
           }
 
           ifstream myfile(nomeDoArquivo);
 
           memset(bufferA, 0, 1024);
-          sleep(1); // Delay pro cliente ficar pronto pra esperar as mensagens
+          sleep(1); // Delay for the client to be ready to receive the messages
 
           string line;
-          if (myfile.is_open()) {
-            while (getline(myfile, line)) {
+          if (myfile.is_open())
+          {
+            while (getline(myfile, line))
+            {
               // cout << "ARQV: " << line << endl;
-
               line += '\0';
-              send(client_sock, line.c_str(), line.size() + 1,
-                   0); // Envia uma linha
-              recv(client_sock, bufferA, 1024,
-                   0); // Espera o ACK do server que recebeu a linha
+              send(client_sock, line.c_str(), line.size()+1, 0); // Sends one line
+              recv(client_sock, bufferA, 1024,0); // Waits the server's ACK that received the line
               memset(bufferA, 0, 1024);
             }
+
             myfile.close();
 
-            for (float i = 0; i < 500000; i++)
-              ; // delay para mandar o fim do arquivo
+            for (float i=0; i<500000; i++)
+              ; // Delay to send the EOF
 
             send(client_sock, endOF.c_str(), endOF.size() + 1, 0);
 
-            string done = "Arquivo enviado com sucesso!";
-            for (float i = 0; i < 100000; i++)
-              ; // delay para
-                // mandar o fim do
-                // arquivo
+            string done = "File sent successfully";
+            for (float i=0; i<100000; i++)
+              ; // Delay to send the EOF
 
             write(client_sock, done.c_str(), done.size() + 1);
           }
         }
       }
+      
     }));
   }
 
